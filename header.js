ea2c36019d3b1ec1c350d72c6f4acc1b34ffe1aa
Rose <r.kutchee@gmail.com>
2024-06-09 14:52:28 -0500

    added a new topic

--- header.js
+++ header.js
@@ -1,10 +1,53 @@
-function loadHeader(headerTitle) {
-    console.log("Loading header...");
-    fetch('../../header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data;
-            document.getElementById('header-title').textContent = headerTitle;
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM fully loaded and parsed");
+    
+    // Load the header
+    fetch('header.html')
+        .then(response => {
+            console.log("Fetch response:", response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
         })
-        .catch(error => console.error('Error loading header:', error));
-}
+        .then(htmlString => {
+            console.log("Fetched HTML string:", htmlString);
+            document.getElementById('header-container').innerHTML = htmlString;
+            document.getElementById('header-title').textContent = "Update title for seek2.html"; // Replace with your actual title
+        })
+        .catch(err => console.error('Error loading header:', err));
+    
+    var audioPlayer = document.getElementById("audio-player");
+    var audioSelect = document.getElementById("audio-select");
+    var downloadAudio = document.getElementById("download-audio");
+
+    audioSelect.addEventListener("change", function() {
+        var selectedAudio = audioSelect.options[audioSelect.selectedIndex].value;
+        if (selectedAudio) {
+            audioPlayer.src = selectedAudio;
+            downloadAudio.href = selectedAudio;
+            downloadAudio.style.display = "inline-block";
+        }
+    });
+
+    // Add event listeners for all dropdown submenus
+    var dropdowns = document.querySelectorAll('.dropdown-submenu');
+
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener('click', function(event) {
+            // Prevent default click behavior
+            event.stopPropagation();
+
+            // Toggle the 'show' class to display the submenu
+            this.querySelector('.dropdown-menu').classList.toggle('show');
+        });
+
+        // Close the dropdown menu when clicking outside
+        document.addEventListener('click', function(event) {
+            var isClickInside = dropdown.contains(event.target);
+            if (!isClickInside) {
+                dropdown.querySelector('.dropdown-menu').classList.remove('show');
+            }
+        });
+    });
+});
